4f9eb7cf7425f11bade701cd877c10628874609c
KavyaGandholi <kavyagandholi9@gmail.com>
2024-06-29 23:47:10 -0400

    updated by locators on the basis of review comments

--- src/main/java/com/qa/opencart/pages/AddCartPage.java
+++ src/main/java/com/qa/opencart/pages/AddCartPage.java
@@ -1,5 +1,15 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class AddCartPage {
 
+	String name = "iMac";
+	
+	By locator = By.id("prodcut");
+	 
+	public void click() {
+		System.out.println("click---" +locator);
+	}
+	
 }
